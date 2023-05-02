27c1156ef7924cff779239c69438065e5177d39a
Bitbox <129644853+Bitboox@users.noreply.github.com>
2023-05-02 03:56:16 -0400

    Add files via upload

---
+++ NPC's/media/lua/client/spawnNpc.lua
@@ -0,0 +1,73 @@
+require "NPCs/ISUI/ISUIHandler"
+require "NPCs/ISUI/ISUIMenu"
+require "NPCs/NPCsProfession"
+
+function spawnNPC(x, y)
+    local square = getCell():getGridSquare(x, y, 0)
+    if square == nil then
+        return
+    end
+
+    local npc = getWorld():createNpc(getRandomProfession(), square)
+    if npc == nil then
+        return
+    end
+
+    npc:setName(getRandomName())
+    npc:setCustomName(npc:getName())
+
+    local traits = getRandomTraits()
+    npc:getTraits():clear()
+    for i = 1, #traits do
+        npc:getTraits():add(traits[i])
+    end
+
+    npc:setModel(getRandomModel())
+
+    npc:getStats():setMaxWeight(math.random(100, 200))
+    npc:getStats():setHunger(0)
+    npc:getStats():setThirst(0)
+    npc:getStats():setPain(0)
+    npc:getStats():setEndurance(1)
+    npc:getStats():setFitness(1)
+    npc:getStats():setStress(0)
+    npc:getStats():setMorale(0)
+
+    npc:Say("Hi there!")
+
+    -- Actualiza el cuadro de diálogo de información del NPC
+    local playerObj = getPlayer()
+    if playerObj and playerObj:getModData().NPCsUIShown then
+        ISUIHandler.updateInfoWindow(npc)
+    end
+end
+
+function getRandomName()
+    local names = {"Alice", "Bob", "Charlie", "Daisy", "Ethan", "Frank", "Gina", "Harry", "Isabella", "Jacob", "Katie", "Liam", "Mia", "Nathan", "Olivia", "Penelope", "Quentin", "Rachel", "Sophie", "Thomas", "Ursula", "Victoria", "Walter", "Xavier", "Yvette", "Zachary"}
+    return names[math.random(#names)]
+end
+
+function getRandomModel()
+    local models = {"female_1", "female_2", "female_3", "male_1", "male_2", "male_3"}
+    return models[math.random(#models)]
+end
+
+function getRandomProfession()
+    return NPCProfessionTable[math.random(#NPCProfessionTable)].type
+end
+
+function getRandomTraits()
+    local traits = {}
+    while #traits < 2 do
+        local trait = TraitFactory.getTrait(getRandomTrait())
+        if trait and not trait:IsNegative() and not trait:IsMutation() then
+            traits[#traits + 1] = trait
+        end
+    end
+    return traits
+end
+
+function getRandomTrait()
+    local traits = {"Brave", "Clumsy", "FastHealer", "Graceful", "Lucky", "Outdoorsman", "ProneToIllness", "SelfDefenseClass", "SlowHealer", "ThickSkinned", "FastReader", "Hiker", "LightEater", "Organized", "ShortSighted", "Asthmatic", "Hemophobic", "WeakStomach"}
+    return traits[math.random(#traits)]
+end
