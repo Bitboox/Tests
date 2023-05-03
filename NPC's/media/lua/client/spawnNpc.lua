afe2c437777ac8b28e486f0135df7a121a9cf8b7
Bitbox <129644853+Bitboox@users.noreply.github.com>
2023-05-02 21:13:29 -0400

    Update spawnNpc.lua

--- NPC's/media/lua/client/spawnNpc.lua
+++ NPC's/media/lua/client/spawnNpc.lua
@@ -1,28 +1,4 @@
-local function spawnNpcs()
-    local players = getOnlinePlayers()
-    for i=0, players:size()-1 do
-        local player = players:get(i)
-        local x, y, z = player:getX(), player:getY(), player:getZ()
-        local square = getCell():getGridSquare(x, y, z)
-        if square ~= nil then
-            local numZombies = square:getZombieCount()
-            local maxZombies = square:getMaxZombies()
-            local numSurvivors = square:getModData().numSurvivors or 0
-            local maxSurvivors = math.floor(maxZombies/10)
-            if numSurvivors < maxSurvivors then
-                local survivorsToSpawn = maxSurvivors - numSurvivors
-                local offset = ZombRand(10, 20)
-                for i=1, survivorsToSpawn do
-                    local angle = ZombRand()*math.pi*2
-                    local offsetx = math.floor(offset * math.cos(angle))
-                    local offsety = math.floor(offset * math.sin(angle))
-                    local newspawnx = x + offsetx
-                    local newspawny = y + offsety
-                    spawnSurvivor(newspawnx, newspawny, z) -- llama a la función spawnSurvivor() del archivo spawnNpc.lua
-                end
-            end
-        end
-    end
-end
-
-Events.OnTick.Add(spawnNpcs)
+Events.OnGameStart.Add(function()
+    SpawnNpcs()
+    addTimer(900000, SpawnNpcs) -- Llama a SpawnNpcs cada 15 minutos (900000ms)
+end)
